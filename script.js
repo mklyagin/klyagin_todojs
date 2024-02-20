71ab338762a0ee3c02db0511749a3d2452546e1c
mklyagin <m.klyagin@tg.dunice.net>
2024-02-20 11:55:47 +0300

    review fix: event listener and eslint

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+const buttonTaskAdd = document.querySelector('#add-task-button')
+const inputTask = document.querySelector('#enter-task-input')
 
-const buttonTaskAdd = document.querySelector('#AddTaskButton')
+const addTaskButtonEventHandler = () => {
+  // eslint-disable-next-line no-console
+  console.log(inputTask.value);
+};
 
-const onClickAddTaskButton = () => {
-    const inputTask = document.querySelector('#InputTask')
-    console.log(inputTask.value);
-}
+buttonTaskAdd.addEventListener('click', addTaskButtonEventHandler);
