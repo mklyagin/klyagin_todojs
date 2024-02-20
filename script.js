2b3127f221e0850fad43540935472291cb970483
mklyagin <m.klyagin@tg.dunice.net>
2024-02-20 12:13:38 +0300

    added press keys support

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const buttonTaskAdd = document.querySelector('#add-task-button')
 const inputTask = document.querySelector('#enter-task-input')
 
-const addTaskButtonEventHandler = () => {
+const ENTER_KEY = 'Enter';
+
+const showTaskInputValue = () => {
   // eslint-disable-next-line no-console
   console.log(inputTask.value);
 };
 
-buttonTaskAdd.addEventListener('click', addTaskButtonEventHandler);
+const inputTaskListener = (event) => {
+  if (event.key === ENTER_KEY) {
+    showTaskInputValue();
+    inputTask.blur();
+  }
+};
+
+buttonTaskAdd.addEventListener('click', showTaskInputValue);
+inputTask.addEventListener('keydown', inputTaskListener);
