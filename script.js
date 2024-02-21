5a620016c1b7adfdc5d20dd4e873dba3e707026a
mklyagin <m.klyagin@tg.dunice.net>
2024-02-21 12:03:00 +0300

    added check-all checkbox and delete all complited button

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const buttonTaskAdd = document.querySelector('#add-task-button');
 const inputTask = document.querySelector('#enter-task-input');
 const ulTask = document.querySelector('#task-list');
+const checkAllCheckbox = document.querySelector('#check-all-checkbox');
+const deleteAllCompletedButton = document.querySelector('#delete-all-completed-button');
 
 const ENTER_KEY = 'Enter';
 
 let taskList = [];
+
+const checkboxStateListener = () => {
+  if (taskList.every((task) => task.isDone)) {
+    checkAllCheckbox.checked = true;
+  } else {
+    checkAllCheckbox.checked = false;
+  }
+};
 
 const taskRender = () => {
   let listOfTasks = '';
   taskList.forEach((task) => {
     listOfTasks
     += `<li id=${task.id}>
-    <input type='checkbox'></input>
+    <input type='checkbox' class='check-task'></input>
     <span>${task.title}</span>
     <button type='button' id='deleteTaskButton'>X</button>
     </li>`;
   });
   ulTask.innerHTML = listOfTasks;
+  document.querySelectorAll('.check-task').forEach((checkbox) => {
+    checkbox.addEventListener('change', checkboxStateListener);
+  });
 };
 
 const addTask = () => {
@@ -43,7 +56,6 @@ const eventUlHandlerListener = (event) => {
     case ('button'):
       taskList = taskList.filter((task) => Number(task.id) !== Number(event.target.parentNode.id));
       taskRender();
-
       break;
     default:
       break;
@@ -57,6 +69,22 @@ const inputTaskListener = (event) => {
   }
 };
 
+const deleteCompleted = () => {
+  taskList = taskList.filter((task) => task.isDone === false);
+  taskRender();
+};
+
+const checkAll = () => {
+  taskList.forEach((task) => {
+    task.isDone = checkAllCheckbox.checked;
+    document.querySelectorAll('.check-task').forEach((checkbox) => {
+      checkbox.checked = checkAllCheckbox.checked;
+    });
+  });
+};
+
 ulTask.addEventListener('click', eventUlHandlerListener);
 buttonTaskAdd.addEventListener('click', addTask);
 inputTask.addEventListener('keydown', inputTaskListener);
+deleteAllCompletedButton.addEventListener('click', deleteCompleted);
+checkAllCheckbox.addEventListener('click', checkAll);
